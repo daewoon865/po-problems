48779bf3d60fac398f7e2a74c7720a3ba7cbf02d
daewoon865 <102994015+daewoon865@users.noreply.github.com>
2022-08-16 17:14:38 -0400

    Create 01-axis.py

---
+++ 2022-08-15/01-axis/01-axis.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readlines()
+T = int(input[0])
+input = input[1:]
+
+def check (word1: str, word2: str) -> bool:
+    track = dict()
+    for i in word1:
+        if track.get(i) is None:
+            track[i] = 1
+        else:
+            track[i] = track[i] + 1
+    
+    for i in word2:
+        if track.get(i) == 1:
+            del (track[i])
+        elif track.get(i) is not None:
+            track[i] = track[i] - 1
+
+    return len(track) == 0
+
+for i in input:
+    i.strip()
+    couple = i.split(" ")
+    couple = [i.lower() for i in couple]
+
+    if check(couple[0], couple[1]) or check(couple[1], couple[0]):
+        print ("YES")
+    else:
+        print("NO")
